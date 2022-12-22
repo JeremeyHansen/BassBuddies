cea2b20e20ef85e0bfb8731fb005408be4e1bc8e
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-12-22 09:39:46 -0500

    set up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import { useState, useEffect } from "react";
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,20 @@
+export default function Navbar(){
+    return (
+        <>
+            <ul>
+                <div>
+                    Home
+                </div>
+                <div>
+                    Cities
+                </div>
+                <div>
+                    Location
+                </div>
+                <div>
+                    Submit
+                </div>
+            </ul>
+        </>
+    )
+}
