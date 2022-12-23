25830ccaf18d44a733b71e0ede26ad34c6516451
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-12-23 16:46:45 -0600

    added nav bar with 3 linkjs to cities contact and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import StarterPage from "./components/StarterPage";
+import { useState, useEffect } from 'react'
+import Navbar from './components/Navbar'
+import StarterPage from './components/StarterPage'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home.js'
+import Cities from './components/Cities.js'
+import Contact from './components/Contact.js'
 
 function App() {
-  const [className, setClassName] = useState("startscreen");
+  const [className, setClassName] = useState('startscreen')
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setClassName("invisible");
-    }, 2900);
-    return () => clearTimeout(timer);
-  }, []);
+      setClassName('invisible')
+    }, 2900)
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <>
       <StarterPage className={className} />
       <Navbar />
+      <div className="com-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cities" element={<Cities />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,26 @@
-import '../css/navbar.css'
-export default function Navbar(){
-    return (
-        <>
-            <ul>
-                <div className="title">
-                    BassBuddy<span>.</span>
-                </div>
-            </ul>
-        </>
-    )
+import "../css/navbar.css";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="nav-title">
+        BassBuddy<span>.</span>
+      </Link>
+      <ul className="" nav>
+        <CustomLink to="/cities">Cities</CustomLink>
+        <CustomLink to="/contact">Submit</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : "hvr-underline-from-right"}>
+      <Link to={to}>{children}</Link>
+    </li>
+  );
 }
