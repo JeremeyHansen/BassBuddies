dbdd4e21a5766e63cbda7ccd7af507e0d44d5a23
Jeremey Hansen <jeremeyhansen@yahoo.com>
2023-03-04 21:41:14 -0800

    css mobile

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -18,16 +18,16 @@ export default function Home() {
         </button>
       </div>
       <div className="note-container">
-      <BsMusicNote className="note-5"/>
+      {/* <BsMusicNote className="note-5"/> */}
       <BsMusicNote className="note-6"/>
-      <BsMusicNote className="note-7"/>
-      <BsMusicNote className="note-8"/>
-      <BsMusicNote className="note-9"/>
+      {/* <BsMusicNote className="note-7"/> */}
+      {/* <BsMusicNote className="note-8"/> */}
+      {/* <BsMusicNote className="note-9"/> */}
       <BsMusicNote className="note-10"/>
-      <BsMusicNoteBeamed className="note2-5"/>
+      {/* <BsMusicNoteBeamed className="note2-5"/> */}
       <BsMusicNoteBeamed className="note2-6"/>
-      <BsMusicNoteBeamed className="note2-7"/>
-      <BsMusicNoteBeamed className="note2-8"/>
+      {/* <BsMusicNoteBeamed className="note2-7"/> */}
+      {/* <BsMusicNoteBeamed className="note2-8"/> */}
       <BsMusicNoteBeamed className="note2-9"/>
       <BsMusicNoteBeamed className="note2-10"/>
       </div>
